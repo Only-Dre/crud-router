cfac5cc7055b9d5f650916c70ad840cf58db2b31
Only-Dre <workofdre@proton.me>
2025-10-20 22:07:25 -0300

    FIX: Correção de importação e declaração de variáveis.

--- src/pages/Lista/Lista.js
+++ src/pages/Lista/Lista.js
@@ -1,23 +1,22 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 import "./Lista.css";
 
 function Lista({ contacts, setContacts }) {
   // Inicializando contatos, edição vazios e telefone
-  const [editingId, setEditingId] = useState("");
+  const [editingId, setEditingId] = useState(null);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
 
-  const [handleSave] = (id) => {
+  const handleSave = (id) => {
     // Salvando no id
     // Varre todos os elementos do contacts
     setContacts(
-        // Faz uma "lista/array", atualizando assim a lista de contatos
-        contacts.map((c) => (
-            c.id === id ? {...c, name : newName, phone : newPhone} : c
-        ))
+      // Faz uma "lista/array", atualizando assim a lista de contatos
+      contacts.map((c) =>
+        c.id === id ? { ...c, name: newName, phone: newPhone } : c
+      )
     );
     setEditingId(null);
-
   };
 
   const handleEdit = (contact) => {
@@ -27,11 +26,11 @@ function Lista({ contacts, setContacts }) {
     setEditingId(contact.id);
     setNewName(contact.name);
     setNewPhone(contact.phone);
-  }
+  };
 
   const handleDelete = (id) => {
-    setContacts(contacts.filter((c) => c.id !== id))
-  }
+    setContacts(contacts.filter((c) => c.id !== id));
+  };
 
   return (
     <div className="lista-container">
@@ -41,14 +40,13 @@ function Lista({ contacts, setContacts }) {
       ) : (
         // SENÃO
         // Função de apresentação de Lista
-        <ul> 
-           {/* Mapeia os elementos cadastrados, verificando e definindo uma chave;
+        <ul>
+          {/* Mapeia os elementos cadastrados, verificando e definindo uma chave;
            Se o editingID for igual ao contact.id, ele já vai renderizar os campos de input, assim carregando o novo nome e telefone
-           Caso não seja igual, executa um 'span' mostrando o nome, telefone e então apresenta o botão de editar ou deletar*/}
+           Caso não seja igual, executa um 'span' mostrando o nome, telefone e então apresenta o botão de editar ou deletar */}
           {contacts.map((contact) => (
-            <li key={contacts.id}>
+            <li key={contact.id} className="contact-item">
               {editingId === contact.id ? (
-                // Div não estilizado
                 <>
                   <input
                     type="text"
@@ -69,8 +67,12 @@ function Lista({ contacts, setContacts }) {
                   <span>
                     {contact.name} - {contact.phone}
                   </span>
-                  <button onClick={() => handleEdit(contact)}>Editar Contato</button>
-                  <button onClick={() => handleDelete(contact)}>Excluir Contato</button>
+                  <button onClick={() => handleEdit(contact)}>
+                    Editar Contato
+                  </button>
+                  <button onClick={() => handleDelete(contact.id)}>
+                    Excluir Contato
+                  </button>
                 </>
               )}
             </li>
